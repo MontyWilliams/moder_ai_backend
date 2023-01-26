47c03c7d2a656c4aaac4368babbfb07217d7c151
Montywilliams <3650@holbertonschool.com>
2023-01-26 07:39:38 -0600

    create startServer function to confirm server is online

--- index.js
+++ index.js
@@ -11,3 +11,9 @@ app.use(express.json({ limit: '50mb'}))
 app.get('/', async (req, res) => {
     res.send('Hello from DALL-E!');
 })
+
+const startServer = async () => {
+    app.listen(8080, () => console.og('Server started on port 8080 '))
+}
+
+startServer();
